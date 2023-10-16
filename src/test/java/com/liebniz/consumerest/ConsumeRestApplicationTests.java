b4f098b9152d96e083ab86dfd2e8e8657e3c73eb
Liebniz <anders.leif.loren@liebniz.com>
2023-10-16 12:07:32 +0200

    Initial commit

---
+++ src/test/java/com/liebniz/consumerest/ConsumeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liebniz.consumerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
